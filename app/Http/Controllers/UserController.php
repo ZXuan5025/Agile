e5d7c789a371dc85d08de1110341e923db1a361f
Kahlok11 <laikl-wm20@student.tarc.edu.my>
2023-03-09 08:38:20 +0800

    user controller - register

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -27,7 +27,38 @@ class UserController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    
+    public function registersubmit(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'phone' => 'required|string|max:255',
+            'email' => 'required|string|max:255',
+            'age' => 'required|string|max:255',
+            'password' => 'required|string|max:255',
+        ]);
+        $data['password'] = Hash::make($data['password']);
+        $data['role'] = "student";
+        $password = $request->input('password');
+        $cpassword = $request->input('cpassword');
+        if($password!= $cpassword){
+            return redirect('registerStudent')->withInput()->with('wrong2' ,'Password and Confirm Password not match');
+        }
+
+        $sEmail = $request->input('email');
+        $sPhone = $request->input('phone');
+        if($this->Repository->checkexistingUser($sEmail, $sPhone)){
+            return redirect('registerStudent')->withInput()->with('wrong' ,'Crash Email or Phone with database');
+        }else{
+        $this->Repository->storeUser($data);
+        $testMailData = [
+            'title' => 'Test Email From AllPHPTricks.com',
+            'body' => 'This is the body of test email.'
+        ];
+
+        Mail::to($sEmail)->send(new SendMail($testMailData));
+        }
+    }
+
 //     public function loginCust(Request $request)
 // {
 //     $cEmail = $request->input('cEmail');
