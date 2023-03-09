1d66b05656f5d313cbf46eafd8ca0efa9d067ea0
YixinTeo <liyin0627@gmail.com>
2023-03-09 08:27:51 +0800

    web

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Student page
+Route::middleware(['auth', 'role:student'])->group(function () {
+    Route::get('/header', function () {
+        return view('student/header');
+    });
+});
+
+//Student page
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/homepageadmin', function () {
+        return view('admin/homepage');
+    });
+});
+
+//register Student page
+Route::get('/registerStudent', function () {
+    return view('student/registerStudent');
+});
+Route::post('/registersubmit',[UserController::class,'registersubmit']);
+
+//login Student page
+Route::get('/loginStudent',[UserController::class, 'redirect'])->name('login');
+
+Route::post('/loginStudentsubmit', [UserController::class, 'login']);
+
+Route::get('/adminAnnouncement', [AnnouncementController::class, 'adminIndex']);
+
+Route::get('/adminTimetable', [CourseController::class, 'adminTimetable']);
+
+Route::get('/updateTimetable/{id}', [CourseController::class, 'updateTimetable']);
+
