9c2fea07a5fb2907e7196c139ef04a88115ac745
ZXuan5025 <choozx-wm20@student.tarc.edu.my>
2023-03-20 11:08:05 +0800

    course controller

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -24,6 +24,12 @@ class CourseController extends Controller{
         return view('student.course', compact('courses'));
     }
 
+    public function adminIndex()
+    {
+        $courses = $this->courseRepository->getAll();
+        return view('admin.adminCourse', compact('courses'));
+    }
+
     public function timetable()
     {
         $timetables = $this->courseRepository->getAll();
@@ -38,7 +44,7 @@ class CourseController extends Controller{
 
     public function updateTimetable(Request $request, $id)
     {
-        $adminTimetable = Course::find($id);
+        $adminTimetable = Course::find($id);   
         $adminTimetable->cStartTime = $request->input('cStartTime');
         $adminTimetable->cEndTime = $request->input('cEndTime');
         $adminTimetable->save();
@@ -46,4 +52,56 @@ class CourseController extends Controller{
         return redirect()->back()->with('');
     }
 
+    public function createCourse(Request $request)
+    {    
+        $data = $request->validate([
+            'staffID' => 'required|string|max:255',
+            'cName' => 'required|string|max:255',
+            'cDescription' => 'required|string|max:255',
+            'cPrice' => 'required|string|max:255',
+            'cDay' => 'required|string|max:255',
+            'cStartTime' => 'required|string|max:255',
+            'cEndTime' => 'required|string|max:255',
+        ]);
+
+        $existingDay = Course::where('cDay', $data['cDay'])
+        ->where('cStartTime', '=', $data['cStartTime'])
+        ->first();
+        if ($existingDay) {
+            // A course with the same cDay value already exists in the database
+            // return response()->json([
+            //     'message' => 'The selected time has been clashed with another class.'
+            // ], 422);
+            return redirect()->back()->withErrors(['message' => 'The selected time has been clashed with another class.']);
+        } else {
+            $this->courseRepository->storeCourse($data);
+            return redirect()->back()->with('success', 'Course created successfully!');
+        }
+    
+    }
+
+    public function updateCourse(Request $request, $id)
+    {   
+        $data = $request->validate([
+            'cName' => 'required|string|max:255',
+            'cDescription' => 'required|string|max:255',
+            'cPrice' => 'required|string|max:255',
+            'cDay' => 'required|string|max:255',
+            'cStartTime' => 'required|string|max:255',
+            'cEndTime' => 'required|string|max:255',
+        ]);
+        $data = $this->courseRepository->updateCourse($data,$id);
+        return redirect()->back()->with('success', 'Course updated successfully!');
+    }
+
+    public function deleteCourse($id)
+    {
+        $course = Course::find($id);
+        if ($course) {
+            $course->delete();
+            return redirect()->back()->with('success', 'Course deleted successfully.');
+        } else {
+            return redirect()->back()->with('error', 'Course not found.');
+        }
+    }
 }
