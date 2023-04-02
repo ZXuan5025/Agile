978b61be845e5f580c496cccdb40b0ee8a47de49
ZXuan5025 <choozx-wm20@student.tarc.edu.my>
2023-04-02 15:17:37 +0800

    upl mat controller

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -3,17 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\FacadePattern\CourseInterface;
 use App\FacadePattern\CourseRepository;
 use App\Models\Enrollment;
 use App\Models\Course;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\File;
+use Illuminate\Support\Facades\Auth;
+use DB;
 
 class CourseController extends Controller{
 
     protected  $courseRepository;
 
-    public function __construct(CourseInterface $courseRepository)
+    public function __construct(CourseRepository $courseRepository)
     {
         $this->courseRepository = $courseRepository;
     }
@@ -27,19 +31,10 @@ class CourseController extends Controller{
     public function adminIndex()
     {
         $courses = $this->courseRepository->getAll();
-        return view('admin.adminCourse', compact('courses'));
-    }
-
-    public function display($id)
-    {
-        $course = Course::findOrFail($id);
-        return view('student.blog', compact('course'));
-    }
-
-    public function timetable()
-    {
-        $timetables = $this->courseRepository->getAll();
-        return view('student.timetable', compact('timetables'));
+        $staffs = User::where('role','staff')
+        ->select('id','name')
+        ->get();
+        return view('admin.adminCourse', compact('courses'),['staffs'=>$staffs]);
     }
 
     public function adminTimetable()
@@ -50,7 +45,7 @@ class CourseController extends Controller{
 
     public function updateTimetable(Request $request, $id)
     {
-        $adminTimetable = Course::find($id);   
+        $adminTimetable = Course::find($id);
         $adminTimetable->cStartTime = $request->input('cStartTime');
         $adminTimetable->cEndTime = $request->input('cEndTime');
         $adminTimetable->save();
@@ -59,15 +54,15 @@ class CourseController extends Controller{
     }
 
     public function createCourse(Request $request)
-    {    
+    {
         $data = $request->validate([
-            'staffID' => 'required|string|max:255',
+            'staffID' => 'required',
             'cName' => 'required|string|max:255',
             'cDescription' => 'required|string|max:255',
-            'cPrice' => 'required|string|max:255',
-            'cDay' => 'required|string|max:255',
-            'cStartTime' => 'required|string|max:255',
-            'cEndTime' => 'required|string|max:255',
+            'cPrice' => 'required',
+            'cDay' => 'required',
+            'cStartTime' => 'required',
+            'cEndTime' => 'required',
         ]);
 
         $existingDay = Course::where('cDay', $data['cDay'])
@@ -83,11 +78,10 @@ class CourseController extends Controller{
             $this->courseRepository->storeCourse($data);
             return redirect()->back()->with('success', 'Course created successfully!');
         }
-    
     }
 
     public function updateCourse(Request $request, $id)
-    {   
+    {
         $data = $request->validate([
             'cName' => 'required|string|max:255',
             'cDescription' => 'required|string|max:255',
@@ -110,4 +104,38 @@ class CourseController extends Controller{
             return redirect()->back()->with('error', 'Course not found.');
         }
     }
+
+    public function display($id)
+    {
+        $course = Course::findOrFail($id);
+        $evaController = new EvaluationController();
+        $evaluations = $evaController->blogIndex($id);
+        return view('student.blog', compact('course'), ['evaluations'=>$evaluations]);
+    }
+
+    public function uploadMaterials(Request $request)
+{
+    $file = $request->file('file');
+    $courseID = $request->input("courseID");
+
+    $filename = $courseID . '.' . $file->getClientOriginalExtension();
+    $path = 'materials/' . $filename;
+
+    // Delete the existing file if it exists
+    if (Storage::exists($path)) {
+        Storage::delete($path);
+    }
+
+    // Upload the new file
+    Storage::putFileAs('materials', $file, $filename);
+
+    return redirect()->back()->with('success', 'File "' . $filename . '" uploaded successfully.');
+}
+
+public function staffCourse(){
+    $id = Auth::user()->id;
+    $courses = Course::where('staffID',$id)->get();
+    return view("admin.myCourse",["courses"=>$courses]);
+}
+
 }
